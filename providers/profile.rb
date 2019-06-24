c21090c82288cdba24841b0a66da030b12426095
Isa F <isaf@labkey.com>
2019-06-24 13:08:31 -0700

    duply 0.2.4 fully plumb arch_dir

--- providers/profile.rb
+++ providers/profile.rb
@@ -52,6 +52,7 @@ action :create do
       max_full_backups: new_resource.keep_full,
       max_fullbkp_age: new_resource.full_every,
       volsize: new_resource.volume_size,
+      arch_dir: new_resource.arch_dir,
       temp_dir: new_resource.temp_dir,
       swift_username: new_resource.swift_username,
       swift_tenant: new_resource.swift_tenant,
